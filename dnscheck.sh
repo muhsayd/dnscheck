f5f65aa7c7f0bb246a5537dd56573f3206b88ee1
muhsayd <muhsayd@hotmail.com>
2017-07-02 15:06:04 +0300

    More Functions Added to show the usage of the script parse the argument to process one domain or loop all domains or looped the deleted zones from the cPanel error log

--- dnscheck.sh
+++ dnscheck.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+usage(){
+echo -e "Usage:
+\t$0\t\t\t\t\t: To loop For The Deleted Zones From The cPanel Error Log
+\t$0 --domain {DOMAIN}\t\t\t: To Process One Domain {DOMAIN}
+\t$0 --all\t\t\t\t: To Loop For All Domains On The Server."
+}
 synczone(){
 	domain="$1"
 	result=`/scripts/dnscluster synczonelocal ${domain} | head -1`
@@ -40,36 +46,71 @@ recreateFreshZone(){
 	domain="$1"
 	user="$2"
 	echo "Trying To reCreate New Zone File for the Domain: ${domain}"
-	ip=`egrep '^ip:' /var/cpanel/userdata/${user}/${domain} 2>/dev/null`
+	ip=`egrep '^ip:' /var/cpanel/userdata/${user}/${domain} | cut -d' ' -f2 2>/dev/null`
 	/scripts/adddns --domain ${domain} --owner ${user} --ip ${ip}
-	installSPFandDKeyIDandSetTTL ${user}
+	if [ -f /var/named/${domain}.db ]; then
+		installSPFandDKeyIDandSetTTL ${user}
+	else
+		rm -f /root/dnscheck/log.$$
+	fi
 }
-for domain in `grep 'Cpanel::NameServer::Conf::BIND::removezone' /usr/local/cpanel/logs/error_log  | cut -d'"' -f2 | sort -u`; 
-	do 
-		if $(egrep -q "^${domain}:" /etc/userdomains 2>/dev/null ) && [ ! -e /var/named/${domain}.db ]; then
-                echo "#################################################################################" | tee -a /root/dnscheck/log.$$
-                echo -e "#\t\t\t\tProcessing ${domain}\t\t\t\t#" | tee -a /root/dnscheck/log.$$
-                echo "#################################################################################" | tee -a /root/dnscheck/log.$$
-			user=`grep -E "^${domain}:" /etc/userdomains | cut -d: -f2` 2>/dev/null
-			if [ "${user}" == ' nobody' ]; then
-				user=''
-				user=`grep -rl "${domain}" /var/cpanel/userdata/ | awk -F'/' '{print $(NF-1)}' | sort -u`
-				if [ "${user}" == '' ]; then
-					echo "No user exist On The System For This Domain" | tee -a /root/dnscheck/log.$$
-					exit 127
-				fi
-			fi
-				echo "Domain: ${domain} Is Owned by ${user}, You need to add zone file for it manually" | tee -a /root/dnscheck/log.$$
-				synczone ${domain} | tee -a /root/dnscheck/log.$$
-				if [ ! -f /var/named/${domain}.db ]; then
-					restoreZoneFromBackup ${domain} ${user} | tee -a /root/dnscheck/log.$$
-				fi
-                        	if [ ! -f /var/named/${domain}.db ]; then
-					echo "Failed To Restore the Zone File From Available Backups" | tee -a /root/dnscheck/log.$$
-                                	recreateFreshZone ${domain} ${user} | tee -a /root/dnscheck/log.$$
-                        	fi
+doTheRestoreWork(){
+	domain="$1"
+	user="$2"
+	echo "Domain: ${domain} Is Owned by ${user}, You need to add zone file for it manually" | tee -a /root/dnscheck/log.$$
+	synczone ${domain} | tee -a /root/dnscheck/log.$$
+	if [ ! -f /var/named/${domain}.db ]; then
+		restoreZoneFromBackup ${domain} ${user} | tee -a /root/dnscheck/log.$$
+	fi
+	if [ ! -f /var/named/${domain}.db ]; then
+		echo "Failed To Restore the Zone File From Available Backups" | tee -a /root/dnscheck/log.$$
+		recreateFreshZone ${domain} ${user} | tee -a /root/dnscheck/log.$$
+	fi
+}
+processDomain(){
+	domain="$1"
+	if $(egrep -q "^${domain}:" /etc/userdomains 2>/dev/null ) && [ ! -e /var/named/${domain}.db ]; then
 		echo "#################################################################################" | tee -a /root/dnscheck/log.$$
-                echo -e "#\t\t\t\t\tDone\t\t\t\t\t#" | tee -a /root/dnscheck/log.$$
-                echo "#################################################################################" | tee -a /root/dnscheck/log.$$
-		fi 
-	done
+		echo -e "#\t\t\t\tProcessing ${domain}\t\t\t\t#" | tee -a /root/dnscheck/log.$$
+		echo "#################################################################################" | tee -a /root/dnscheck/log.$$
+		user=`grep -E "^${domain}:" /etc/userdomains | cut -d: -f2` 2>/dev/null
+		if [ "${user}" == ' nobody' ]; then
+			user=''
+			user=`grep -rl "${domain}" /var/cpanel/userdata/ | awk -F'/' '{print $(NF-1)}' | sort -u`
+			if [ "${user}" == '' ]; then
+				echo "No user exist On The System For This Domain" | tee -a /root/dnscheck/log.$$
+			else
+			doTheRestoreWork ${domain} ${user}
+		fi
+	else
+		doTheRestoreWork ${domain} ${user}
+	fi
+	echo "#################################################################################" | tee -a /root/dnscheck/log.$$
+	echo -e "#\t\t\t\t\tDone\t\t\t\t\t#" | tee -a /root/dnscheck/log.$$
+	echo "#################################################################################" | tee -a /root/dnscheck/log.$$
+	if [ ! -f /var/named/${domain}.db ]; then
+		rm -f /root/dnscheck/log.$$
+	fi
+	fi
+}
+loopDeletedZones(){
+	for domain in `grep 'Cpanel::NameServer::Conf::BIND::removezone' /usr/local/cpanel/logs/error_log  | cut -d'"' -f2 | sort -u`; 
+		do 
+			processDomain ${domain}
+		done
+}
+mainFunction(){
+	if [ "$#" -eq 0 ]; then
+		loopDeletedZones
+	elif  [ "$#" -eq 1 ] && [ "$1" == '--all' ]; then
+		for domain in `cat /etc/userdomains | cut -d: -f1`
+		do
+			processDomain ${domain}
+		done
+	elif [ "$#" -eq 2 ] && [ "$1" == '--domain' ]; then
+		domain="$2"
+	else
+		usage
+	fi
+}
+mainFunction "$@"
